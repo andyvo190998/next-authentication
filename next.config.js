f736e61d2d3306bd17061e3b8823a9c2271ca242
Andy Vo <andyvo@Andys-MacBook-Pro.local>
2023-03-27 08:39:21 +0200

    modify config file

--- next.config.js
+++ next.config.js
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  future: {
+    webpack5: true,
+  },
   env: {
-      NEXTAUTH_URL: process.env.NEXTAUTH_URL,
+    NEXTAUTH_URL: process.env.NEXTAUTH_URL,
     //   NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
     MONGODB_URI: process.env.MONGODB_URI,
   },
